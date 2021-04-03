06d0f6410f8dba7bdc4eacc58697cf0ccdd4e617
Khoa1468 <69295064+Khoa1468@users.noreply.github.com>
2021-04-03 19:39:14 +0700

    create News.controller.js File

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -3,17 +3,6 @@ const sitesRouter = require('./sites.route')
 
 function route(app){  
     app.use('/news', newsRouter);
-    // app.get("/", (req, res) => {
-    //     res.render("home");
-    // });
-    // app.get("/trang-chu", (req, res) => {
-    //     res.render("moi");
-    // });
-    // // app.get("/tin-tuc", (req, res) => {
-    // //     res.render("news");
-    // // });
-    // app.get("/search", (rep, res) => {
-    //     res.render("search");
     app.use('/', sitesRouter);
 };
 
