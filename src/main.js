59d5f5c728cb0d278186a054e6573a71d8870e1d
Khoa1468 <69295064+Khoa1468@users.noreply.github.com>
2021-04-03 18:19:35 +0700

    Commit File main.js

--- src/main.js
+++ src/main.js
@@ -1,35 +1,38 @@
-const express = require('express')
-const path = require('path')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const app = express()
-const port = 3000
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
-app.engine('hbs', handlebars({
-    extname: '.hbs'
-}))
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources\\views'))
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources\\views"));
 // Use \\ for Windows, / for Mac
-console.log(`PATH: ${path.join(__dirname, 'resources\\views')}`)
+console.log(`PATH: ${path.join(__dirname, "resources\\views")}`);
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-app.get('/trang-chu', (req, res) => {
-    res.render('moi')
-})
-app.get('/tin-tuc', (req, res) => {
-    res.render('news')
-})
-app.get('/search', (rep, res) => {
-  res.render('search')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/trang-chu", (req, res) => {
+  res.render("moi");
+});
+app.get("/tin-tuc", (req, res) => {
+  res.render("news");
+});
+app.get("/search", (rep, res) => {
+  res.render("search");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
